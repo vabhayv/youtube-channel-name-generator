50aa688b598e326717e61c325fcba9ce15c24cbf
Abhay Lodhi <156104058+vabhayv@users.noreply.github.com>
2024-01-10 01:36:23 +0530

    Updated a bit main.py
    just merge 2 lines into 1 :)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Welcome to the Youtube Channel Name Generator")
-k=input("Tap Enter to continue")
-first=input("Write a one word which you use to praise yourself (for example - amazing etc..) :\n")
+print("" + input("Welcome to the Youtube Channel Name Generator\nTap Enter to continue"))
+first=input("Write one word which you use to praise yourself (for example - amazing etc..) :\n")
 second=input("What is your first name ?\n")
 print(f"You can name your Channel : {first} {second}")
 
