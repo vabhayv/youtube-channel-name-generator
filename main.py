92c8de63c2bcc48f2a4e3a1f6707ee17bc4ab5a3
Abhay Lodhi <156104058+vabhayv@users.noreply.github.com>
2024-01-10 01:24:44 +0530

    code for the youtube channel name generator
    Hello Everyone!!
    This is the simple python code I have created which generate youtube channel name by taking some inputs from users.
    Hope you'll like this ;)
    Have a great day

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Youtube Channel Name Generator")
+k=input("Tap Enter to continue")
+first=input("Write a one word which you use to praise yourself (for example - amazing etc..) :\n")
+second=input("What is your first name ?\n")
+print(f"You can name your Channel : {first} {second}")
+
